62a384fcbc66069a46b1924a1a1e6d67e0d2f49d
naweesha <naweesalwathura@gmail.com>
2019-03-11 09:28:46 +0530

    test view and counting onlu + values

--- app/src/main/java/lk/ac/kln/countdown/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdown/MainActivity.java
@@ -6,26 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import lk.ac.kln.countdown.R;
 
 public class MainActivity extends AppCompatActivity {
-    private  int counter =100;
+    private  int counter =99;
+    private static  final String CURRENT_COUNTER = "counter";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+        if (saveInstanceState != null){
+            counter = saveInstanceState.getInt(CURRENT_COUNTER);
+        }
         countDown();
     }
 
-    private void countDown(){
-        final TextView textView = findViewById(R.id.textView);
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        saveInstanceState.putInt(CURRENT_COUNTER,counter);
+    }
 
+    private void countDown(){
         final Handler handler= new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-
+                final TextView textView = findViewById(R.id.textView);
                 textView.setText(Integer.toString(counter));
+
+                if (counter == 0) {
+                    counter = 99;
+
+                }
                 counter--;
                 handler.postDelayed(this,1000);
 
